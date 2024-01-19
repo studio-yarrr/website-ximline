36c317f6d8b09b13efc7a0d8fc7131ae885c64b4
hanomag <>
2024-01-19 11:08:37 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,6 @@ const path = {
       [srcPath +
         "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
       '!' + srcPath + 'assets/sprite/*.svg',
-      '!' + srcPath + 'assets/images/favicon/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}', 
       ],
     fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
     sprites: srcPath + 'assets/sprite/*.svg',
@@ -65,7 +64,6 @@ const path = {
       [srcPath +
       "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
       '!' + srcPath + 'assets/sprite/*.svg',
-      '!' + srcPath + 'assets/images/favicon/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}', 
       ],
     fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
   },
