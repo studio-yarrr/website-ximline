60718bd51dd011037704b6d55d534f7c07d3c5dc
Sanexis <amdrech1406@gmail.com>
2024-09-02 12:09:08 +0300

    feat(gulp, packcage): update files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { src, dest } = require("gulp");
+const { src, dest, watch, series, parallel } = require("gulp");
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const cssbeautify = require("gulp-cssbeautify");
@@ -8,56 +8,51 @@ const removeComments = require("gulp-strip-css-comments");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
 const rigger = require("gulp-rigger");
 const plumber = require("gulp-plumber");
 const panini = require("panini");
 const imagemin = require("gulp-imagemin");
 const del = require("del");
+const notify = require("gulp-notify");
 const browserSync = require("browser-sync").create();
-const purgecss = require("gulp-purgecss");
-// const tailwindcss = require("tailwindcss");
-const postcss = require('gulp-postcss');
-const fileInclude = require('gulp-file-include');
-const pug = require('gulp-pug');
-const argv = require('yargs').argv;
-const footer = require('gulp-footer');
-const svgSprite = require('gulp-svg-sprite')
-const replace = require('gulp-string-replace');
 
 /* Paths */
 const srcPath = "src/";
 const distPath = "dist/";
+
 const path = {
   build: {
     html: distPath,
-    php: distPath,
+    php: distPath, // Добавлено
     js: distPath + "assets/js/",
     css: distPath + "assets/css/",
     images: distPath + "assets/images/",
     fonts: distPath + "assets/fonts/",
-    vendorcss: distPath + "assets/css/vendor/",
-    sprites: distPath + "assets/images/"
+    videos: distPath + "assets/videos/",
   },
   src: {
     html: srcPath + "*.html",
-    php: srcPath + "*.php",
+    php: srcPath + "*.php", // Добавлено
     js: srcPath + "assets/js/*.js",
     css: srcPath + "assets/scss/*.scss",
-    vendorcss: srcPath + "assets/js/components/*.css",
-    pug: srcPath + "*.pug",
-    images: srcPath + "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+    images:
+      srcPath +
+      "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
     fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
-    sprites: srcPath + 'assets/sprite/*.svg',
+    videos: srcPath + "assets/videos/**/*.{mp4,webm}",
   },
   watch: {
     html: srcPath + "**/*.html",
-    php: srcPath + "**/*.php",
+    php: srcPath + "**/*.php", // Добавлено
     js: srcPath + "assets/js/**/*.js",
     css: srcPath + "assets/scss/**/*.scss",
-    vendorcss: srcPath + "assets/js/components/*.css",
-    pug: srcPath + "*.pug",
-    images: srcPath + "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+    images:
+      srcPath +
+      "assets/images/**/*.{jpg,png,svg,glb,gif,ico,webp,webmanifest,xml,json}",
     fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+    videos: srcPath + "assets/videos/**/*.{mp4,webm}",
   },
   clean: "./" + distPath,
 };
@@ -70,21 +65,6 @@ function serve() {
       baseDir: "./" + distPath,
     },
   });
-}
-
-function sprites() {
-  return src(path.src.sprites)
-    .pipe(svgSprite({
-      mode: {
-        stack: {
-          sprite: "../sprite.svg"
-        }
-      },
-    }
-    ))
-    .pipe(replace(new RegExp('stroke=".*?"|fill=".*?"|opacity=".*?"', 'g'), ''))
-    .pipe(dest(path.build.sprites))
-    .pipe(browserSync.stream())
 }
 
 function html(cb) {
@@ -100,29 +80,14 @@ function html(cb) {
         data: srcPath + "data/",
       })
     )
-    .pipe(fileInclude({
-      prefix: '@',
-      basepath: '@file',
-    }))
     .pipe(dest(path.build.html))
     .pipe(browserSync.reload({ stream: true }));
-
-  cb();
 }
 
 function php(cb) {
-  return src(path.src.php, { base: srcPath })
-    .pipe(dest(path.build.php))
+  return src(path.src.php, { base: srcPath }) // Используем исходный путь для PHP файлов
+    .pipe(dest(path.build.php)) // Копируем файлы в папку dist
     .pipe(browserSync.reload({ stream: true }));
-}
-
-function pugs(cb) {
-  return src(path.src.pug, { base: srcPath })
-    .pipe(pug())
-    .pipe(dest(path.build.html))
-    .pipe(browserSync.reload({ stream: true }))
-
-  cb()
 }
 
 function css(cb) {
@@ -132,65 +97,11 @@ function css(cb) {
         includePaths: "./node_modules/",
       })
     )
-    // .pipe(postcss([tailwindcss("./tailwind.config.js")]))
     .pipe(
       autoprefixer({
         cascade: true,
       })
     )
-    .pipe(cssbeautify())
-    // .pipe(
-    //   cssnano({
-    //     zindex: false,
-    //     discardComments: {
-    //       removeAll: true,
-    //     },
-    //   })
-    // )
-    .pipe(removeComments())
-    // .pipe(dest(path.build.css))
-    // .pipe(
-    //   rename({
-    //     suffix: ".min",
-    //     extname: ".css",
-    //   })
-    // )
-    .pipe(dest(path.build.css))
-    .pipe(browserSync.reload({ stream: true }));
-
-  cb();
-}
-
-function vendorcss(cb) {
-  return src(path.src.vendorcss, { base: srcPath + "assets/js/components/" })
-    .pipe(dest(path.build.vendorcss))
-}
-
-function cleanCss(cb) {
-  return src(path.src.css, { base: srcPath + "assets/scss/" })
-    .pipe(
-      sass({
-        includePaths: "./node_modules/",
-      })
-    )
-    // .pipe(postcss([tailwindcss("./tailwind.config.js")]))
-    .pipe(
-      purgecss({
-        content: ["src/**/*.{html,js,php}"],
-        safelist: ['hello'],
-        defaultExtractor: (content) => {
-          const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-          const innerMatches =
-            content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-          return broadMatches.concat(innerMatches);
-        },
-      })
-    )
-    // .pipe(
-    //   autoprefixer({
-    //     cascade: true,
-    //   })
-    // )
     .pipe(cssbeautify())
     .pipe(dest(path.build.css))
     .pipe(
@@ -202,37 +113,14 @@ function cleanCss(cb) {
       })
     )
     .pipe(removeComments())
-    .pipe(dest(path.build.css))
-    .pipe(browserSync.reload({ stream: true }));
-
-  cb();
-}
-
-function cssWatch(cb) {
-  return src(path.src.css, { base: srcPath + "assets/scss/" })
     .pipe(
-      sass({
-        includePaths: "./node_modules/",
+      rename({
+        suffix: ".min",
+        extname: ".css",
       })
     )
-    // .pipe(postcss([tailwindcss("./tailwind.config.js")]))
-    // .pipe(cssbeautify())
-    // .pipe(removeComments())
-    // .pipe(
-    //   autoprefixer({
-    //     cascade: true,
-    //   })
-    // )
-    // .pipe(
-    //   rename({
-    //     suffix: ".min",
-    //     extname: ".css",
-    //   })
-    // )
     .pipe(dest(path.build.css))
     .pipe(browserSync.reload({ stream: true }));
-
-  cb();
 }
 
 function js(cb) {
@@ -240,17 +128,6 @@ function js(cb) {
     .pipe(rigger())
     .pipe(dest(path.build.js))
     .pipe(browserSync.reload({ stream: true }));
-
-  cb();
-}
-
-function jsWatch(cb) {
-  return src(path.src.js, { base: srcPath + "assets/js/" })
-    .pipe(rigger())
-    .pipe(dest(path.build.js))
-    .pipe(browserSync.reload({ stream: true }));
-
-  cb();
 }
 
 function images(cb) {
@@ -267,151 +144,32 @@ function images(cb) {
     )
     .pipe(dest(path.build.images))
     .pipe(browserSync.reload({ stream: true }));
-
-  cb();
 }
 
-function imagesWatch(cb) {
-  return src(path.src.images)
-    .pipe(dest(path.build.images))
+function videos(cb) {
+  return src(path.src.videos)
+    .pipe(dest(path.build.videos))
     .pipe(browserSync.reload({ stream: true }));
-
-  cb();
 }
 
 function fonts(cb) {
   return src(path.src.fonts)
     .pipe(dest(path.build.fonts))
     .pipe(browserSync.reload({ stream: true }));
-
-  cb();
 }
 
 function clean(cb) {
   return del(path.clean);
-
-  cb();
 }
-
-function cleanWithoutImg(cb) {
-  return del([`!dist/**/images/**`, 'dist/**/fonts/**', 'dist/**/css/**', 'dist/**/js/**', 'dist/index.html'])
-}
-
-function newFile() {
-  if (argv.page?.length) {
-    const arr = argv.page.split(' ');
-
-    arr.forEach(element => {
-      return src('src/assets/empty.html')
-        .pipe(rename(() => {
-          return {
-            dirname: '.',
-            basename: element,
-            extname: '.html',
-          }
-        }))
-        .pipe(dest('src'), { overwrite: false, append: true })
-        .pipe(rename(() => {
-          return {
-            dirname: '.',
-            basename: element,
-            extname: '.scss',
-          }
-        }))
-        .pipe(dest('src/assets/scss/blocks'), { overwrite: false, append: true })
-    });
-
-    return Promise.resolve('значение игнорируется');
-
-  } else if (argv.vendor?.length) {
-
-    const arr = argv.vendor.split(' ');
-    arr.forEach(element => {
-      return src('src/assets/empty.html')
-        .pipe(rename(() => {
-          return {
-            dirname: '.',
-            basename: element,
-            extname: '.scss',
-          }
-        }))
-        .pipe(dest('src/assets/scss/blocks'), { overwrite: false, append: true })
-    })
-    return Promise.resolve('значение игнорируется');
-  } else if (argv.partial?.length) {
-    const arr = argv.partial.split(' ');
-
-    arr.forEach(element => {
-      return src('src/assets/empty.html')
-        .pipe(rename(() => {
-          return {
-            dirname: '.',
-            basename: element,
-            extname: '.html',
-          }
-        }))
-        .pipe(dest('src/partials'), { overwrite: false, append: true })
-        .pipe(rename(() => {
-          return {
-            dirname: '.',
-            basename: element,
-            extname: '.scss',
-          }
-        }))
-        .pipe(dest('src/assets/scss/blocks'), { overwrite: false, append: true })
-    });
-
-    return Promise.resolve('значение игнорируется');
-  } else {
-    return Promise.resolve('значение игнорируется');
-  }
-}
-
-function toEnd() {
-  if (argv.page?.length) {
-    const arr = argv.page.split(' ');
-
-    gulp.src('src/assets/scss/_importsBlocks.scss')
-      .pipe(footer(arr.map(el => ' @import \'./blocks/' + el + '.scss\';').join(' ')))
-      .pipe(cssbeautify())
-      .pipe(gulp.dest('src/assets/scss/'), { overwrite: true, append: false });
-    gulp.src('src/index.html')
-      .pipe(footer(arr.map(el => `\n<li><a href="${el}.html" class="_progress__link">${el}</a></li>`).join(' ')))
-      .pipe(gulp.dest('src/'), { overwrite: true, append: false });
-
-    return Promise.resolve('значение игнорируется');
-  } else if (argv.vendor?.length) {
-    const arr = argv.vendor.split(' ');
-    gulp.src('src/assets/scss/_importsBlocks.scss')
-      .pipe(footer(arr.map(el => ' @import \'./blocks/' + el + '.scss\';').join(' ')))
-      .pipe(cssbeautify())
-      .pipe(gulp.dest('src/assets/scss/'), { overwrite: true, append: false });
-    return Promise.resolve('значение игнорируется');
-  } else if (argv.partial?.length) {
-    const arr = argv.partial.split(' ');
-    gulp.src('src/assets/scss/_importsBlocks.scss')
-      .pipe(footer(arr.map(el => ' @import \'./blocks/' + el + '.scss\';').join(' ')))
-      .pipe(cssbeautify())
-      .pipe(gulp.dest('src/assets/scss/'), { overwrite: true, append: false });
-    return Promise.resolve('значение игнорируется');
-  } else {
-    return Promise.resolve('значение игнорируется');
-  }
-}
-
 
 function watchFiles() {
-  gulp.watch([path.watch.html], gulp.series(html, cssWatch));
-  // gulp.watch([path.watch.pug], pugs)
-  // gulp.watch([path.watch.css], vendorcss);
-  gulp.watch([path.watch.php], php)
-  gulp.watch([path.watch.css], cssWatch);
-  gulp.watch([path.watch.js], jsWatch);
-  gulp.watch([path.watch.images], imagesWatch);
-  // gulp.watch([path.watch.images], images);
-  gulp.watch([path.watch.fonts], fonts);
-  // gulp.watch(['./tailwind.config.js'], gulp.series(html, cssWatch))
-  gulp.watch([path.src.sprites], sprites);
+  watch([path.watch.html], html);
+  watch([path.watch.php], php); // Добавлено
+  watch([path.watch.css], css);
+  watch([path.watch.js], js);
+  watch([path.watch.images], images);
+  watch([path.watch.videos], videos);
+  watch([path.watch.fonts], fonts);
 }
 
 function imagesWithoutMin() {
@@ -420,30 +178,23 @@ function imagesWithoutMin() {
     .pipe(browserSync.reload({ stream: true }));
 }
 
-const buildOld = gulp.series(clean, gulp.parallel(html, php, css, vendorcss, js, images, fonts));
-const start = gulp.series(cleanWithoutImg, gulp.parallel(html, php, css, js, fonts, sprites));
-const watch = gulp.parallel(start, watchFiles, serve);
-const build = gulp.parallel(buildOld, watchFiles, serve);
-const buildCleanCSS = gulp.series(clean, gulp.parallel(html, cleanCss, js, images, fonts));
-const make = gulp.series(gulp.parallel(newFile, toEnd));
-const buildNMin = gulp.series(clean, html, css, js, images, fonts, sprites);
+const build = series(
+  clean,
+  parallel(html, php, css, js, images, videos, fonts) // Добавлено php
+);
+const dev = parallel(build, watchFiles, serve);
+const buildNMin = gulp.series(clean, html, css, js, imagesWithoutMin, fonts);
 
 /* Exports Tasks */
-
-exports.make = make;
 exports.html = html;
-exports.php = php;
+exports.php = php; // Добавлено
 exports.css = css;
 exports.js = js;
 exports.images = images;
+exports.videos = videos;
 exports.fonts = fonts;
 exports.clean = clean;
 exports.build = build;
-exports.watch = watch;
-exports.default = watch;
-exports.cleanWithoutImg = cleanWithoutImg
-exports.start = start
-exports.buildCleanCSS = buildCleanCSS
-exports.buildNMin = buildNMin
-exports.sprites = sprites;
-
+exports.dev = dev;
+exports.default = dev;
+exports.buildNMin = buildNMin;
